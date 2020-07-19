d57cd51b50a5fd63482cde66b36bab7e1775108b
Umutbek <karimovumutbek@gmail.com>
2020-07-19 13:19:29 +0200

    Create tags endpoint

---
+++ app/user/views.py
@@ -0,0 +1,28 @@
+from rest_framework import generics, authentication, permissions
+#use to create API view it allows us to easily make a API that creates an obj in a db using serialize we are going to provide
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create new user in syste"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authentication user"""
+        return self.request.user
